1d2175590f4a7fc6c115d17932f987452642c6e7
bouchrabasri <bouchraazrour2@gmail.com>
2024-01-12 13:23:49 +0200

    I add read from output file function

--- cpe351.cpp
+++ cpe351.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #define LINE_MAX 1024
 using namespace std;
 
@@ -14,22 +15,24 @@ struct process
 
 double wTime = 0;
 double avgWaitingTime = 0;
-int count = 0;
 
 struct process* createProcess(int, double, double, double);
 struct process* insertProcess(struct process*, int, double, double, double);
 int MainMenu(string, string);
 int SchedulingMenu();
+int preemptive_mode();
 void swap(struct process*, struct process*);
 struct process* sortArrivalTime(struct process*);
 struct process* sortBurstTime(struct process*);
 struct process* sortpriority(struct process*);
-void FCFS(struct process*);
-void SJF_NP(struct process*);
-void priority_NP(struct process*);
+void FCFS(struct process*, FILE*);
+void SJF_NP(struct process*, FILE*);
+void priority_NP(struct process*, FILE*);
 int size(struct process*);
+void showResult(string, string, struct process*, FILE*);
+void readOutputFile(const char*);
 
-int main()
+int main(int argc,char *argv[])
 {
     if(argc!=5){
         cout << "Invalid number of arguments." << endl;
@@ -61,7 +64,6 @@ int main()
         char colon;
         istringstream iss(line);
         iss >> i[0] >> colon >> i[1] >> colon >> i[2];
-
         p = insertProcess(p, x, i[0], i[1], i[2]);
         x++;
     }
@@ -89,8 +91,6 @@ int main()
                         break;
                     case 5:
                         schMethod = "Round Robin";
-                        break;
-                    default:
                         break;
                 }
                 break;
@@ -123,33 +123,10 @@ int main()
                 }
                 break;
             case 3:
-                if(schMethod == "FCFS")
-                {
-                    FCFS(p);
-                }
-                else if(schMethod == "SJF" && preMode == "Off")
-                {
-                    SJF_NP(p);
-                }
-                else if(schMethod == "SJF" && preMode == "On")
-                {
-                    cout << "This function is not implemented yet.";
-                }
-                else if(schMethod == "Priority" && preMode == "Off")
-                {
-                    priority_NP(p);
-                }
-                else if(schMethod == "Priority" && preMode == "On")
-                {
-                    cout << "This function is not implemented yet.";
-                }
-                else if(schMethod == "Round Robin")
-                {
-                    cout << "This function is not implemented yet.";
-                }
+                showResult(schMethod, preMode, p, output);
                 break;
         }
-    }while(menu < 4);
+    }while(menu <= 4);
 
     return 0;
 }
@@ -205,6 +182,17 @@ int SchedulingMenu() //The function displays the menu options and waits for user
     cout << "Option > ";
     cin >> schedulingMethod;
     return schedulingMethod;
+}
+
+int preemptive_mode()
+{
+    int pMode;
+    cout << "preemptive MODE" << endl;
+    cout << "1.off" << endl;
+    cout << "2.on" << endl;
+    cout << "Option > ";
+    cin >> pMode;
+    return pMode;
 }
 
 void swap(struct process* processHeader, struct process* nextProcess)
@@ -297,12 +285,16 @@ struct process* sortpriority(struct process* processHeader)
     return processHeader;
 }
 
-void FCFS(struct process* processHeader)
+void FCFS(struct process* processHeader, FILE* output)
 {
     double completionTime = 0; 
     double turn_around_time = 0;
+    int count = 0;
+    double sum = 0;
     cout << "Scheduling Method: First Come First Served" << endl;
+    fprintf(output, "Scheduling Method: First Come First Served\n");
     cout << "Process Waiting Time:" << endl;
+    fprintf(output, "Process Waiting Time:\n");
     processHeader = sortArrivalTime(processHeader);
     while(processHeader != NULL)
     {
@@ -310,35 +302,42 @@ void FCFS(struct process* processHeader)
         turn_around_time = completionTime - processHeader->arrivalTime; 
         wTime = turn_around_time - processHeader->burstTime;
         cout << "P" << processHeader->ID << ": " << wTime << " ms" << endl;
-        avgWaitingTime += wTime;
+        fprintf(output, "P %d: %f ms\n", processHeader->ID, wTime);
+        sum += wTime;
         processHeader = processHeader->next;
         count++; // This variable calculate the number of processes in the list.
     }
     if(count != 0)
     {
-        avgWaitingTime = avgWaitingTime / count;
+        avgWaitingTime = sum / count;
     }
     else
     {
         avgWaitingTime = 0; // List is empty.
     }
-    cout << " Average Waiting Time: " << avgWaitingTime << " ms" << endl;
+    cout << "Average Waiting Time: " << avgWaitingTime << " ms" << endl;
+    fprintf(output, "Average Waiting Time: %f \n ", avgWaitingTime);
 }
 
-void SJF_NP(struct process* processHeader)
+void SJF_NP(struct process* processHeader, FILE* output)
 {
     int completionTime = 0;
-    int Turn_around_time = 0;
+    int turn_around_time = 0;
+    double sum = 0;
+    double count = 0;
     cout << "Scheduling Method: Shortest Job First - Non Preemptive" << endl;
+    fprintf(output, "Scheduling Method: Shortest Job First - Non Preemptive\n");
     cout << "Process Waiting Time:" << endl;
+    fprintf(output, "Process Waiting Time:\n");
     processHeader = sortArrivalTime(processHeader);
     while(processHeader != NULL)
     {
-        completionTime += processHeader->burstTime; 
+        completionTime += processHeader->burstTime; // Adding the burst time of each process that has finished. 
         turn_around_time = completionTime - processHeader->arrivalTime; 
         wTime = turn_around_time - processHeader->burstTime;
         cout << "P" << processHeader->ID << ": " << wTime << " ms" << endl;
-        avgWaitingTime += wTime;
+        fprintf(output, "P %d: %f ms\n", processHeader->ID, wTime);
+        sum += wTime;
         processHeader = processHeader->next;
         count++;
         break;
@@ -348,40 +347,47 @@ void SJF_NP(struct process* processHeader)
     {
         if(processHeader->next != NULL)
         {
-            completionTime += processHeader->burstTime;
-            turn_around_time = completionTime - processHeader->arrivalTime;
-            wTime = turn_around_time - processheader->burstTime;
-            cout << "P" << processHeader->ID << ": " << wTime << " ms" << endl;                avgWaitingTime += wTime;
-            avgWaitingTime += wTime;
+                completionTime += processHeader->burstTime;
+                turn_around_time = completionTime - processHeader->arrivalTime;
+                wTime = turn_around_time - processHeader->burstTime;
+                cout << "P" << processHeader->ID << ": " << wTime << " ms" << endl;
+                fprintf(output, "P %d: %f ms\n", processHeader->ID, wTime);
+                sum += wTime;
         }
         else if(processHeader->next == NULL)
         {
-            completionTime += processHeader->burstTime;
-            turn_around_time = completionTime - processHeader->arrivalTime;
-            wTime = turn_around_time - processHeader->burstTime;
-            cout << "P" << processHeader->ID << ": " << wTime << " ms" << endl;
-            avgWaitingTime += wTime;
+                completionTime += processHeader->burstTime;
+                turn_around_time = completionTime - processHeader->arrivalTime;
+                wTime = turn_around_time - processHeader->burstTime;
+                cout << "P" << processHeader->ID << ": " << wTime << " ms" << endl;
+                fprintf(output, "P %d: %f ms\n", processHeader->ID, wTime);
+                sum += wTime;
         }
         processHeader = processHeader->next;
         count++;
     }
     if(count != 0)
     {
-        avgWaitingTime = avgWaitingTime / count;
+        avgWaitingTime = sum / count;
     }
     else
     {
         avgWaitingTime = 0; // List is empty.
     }
-    cout << " Average Waiting Time: " << avgWaitingTime << " ms" << endl;
+    cout <<"Average Waiting Time: " << avgWaitingTime << " ms" << endl;
+    fprintf(output, "Average Waiting Time: %f ms \n ", avgWaitingTime);
 }
 
-void priority_NP(struct process* processHeader)
+void priority_NP(struct process* processHeader, FILE* output)
 {
     int completionTime = 0;
     int turn_around_time = 0;
+    int count = 0;
+    double sum = 0;
     cout << "Scheduling Method: Priority Scheduling - Non Preemptive" << endl;
+    fprintf(output, "Scheduling Method: Priority Scheduling - Non Preemptive\n");
     cout << "Process Waiting Time:" << endl;
+    fprintf(output, "Process Waiting Time:\n");
     processHeader = sortArrivalTime(processHeader);
     while(processHeader != NULL)
     {
@@ -389,7 +395,8 @@ void priority_NP(struct process* processHeader)
         turn_around_time = completionTime - processHeader->arrivalTime; 
         wTime = turn_around_time - processHeader->burstTime;
         cout << "P" << processHeader->ID << ": " << wTime << " ms" << endl;
-        avgWaitingTime += wTime;
+        fprintf(output, "P %d: %f ms\n", processHeader->ID, wTime);
+        sum += wTime;
         processHeader = processHeader->next;
         count++;
         break;
@@ -403,7 +410,8 @@ void priority_NP(struct process* processHeader)
                 turn_around_time = completionTime - processHeader->arrivalTime;
                 wTime = turn_around_time - processHeader->burstTime;
                 cout << "P" << processHeader->ID << ": " << wTime << " ms" << endl;
-                avgWaitingTime += wTime;
+                fprintf(output, "P %d: %f ms\n", processHeader->ID, wTime);
+                sum += wTime;
         }
         else if(processHeader->next == NULL)
         {
@@ -411,20 +419,22 @@ void priority_NP(struct process* processHeader)
                 turn_around_time = completionTime - processHeader->arrivalTime;
                 wTime = turn_around_time - processHeader->burstTime;
                 cout << "P" << processHeader->ID << ": " << wTime << " ms" << endl;
-                avgWaitingTime += wTime;
+                fprintf(output, "P %d: %f ms\n", processHeader->ID, wTime);
+                sum += wTime;
         }
         processHeader = processHeader->next;
         count++;
     }
     if(count != 0)
     {
-        avgWaitingTime = avgWaitingTime / count;
+        avgWaitingTime = sum / count;
     }
     else
     {
         avgWaitingTime = 0; // List is empty.
     }
-    cout << " Average Waiting Time: " << avgWaitingTime << " ms" << endl;
+    cout << "Average Waiting Time: " << avgWaitingTime << " ms" << endl;
+    fprintf(output, "Average Waiting Time: %f ms \n", avgWaitingTime);
 }
 
 int size(struct process *processHeader)
@@ -436,4 +446,61 @@ int size(struct process *processHeader)
 		processHeader = processHeader->next;
 	}
 	return x;	
+}
+
+void showResult(string Method, string Mode,struct process* p, FILE* output)
+{
+    if(Method == "FCFS" && Mode == "off")
+    {
+        FCFS(p,output);
+    }
+    else if(Method == "FCFS" && Mode == "On")
+    {
+        cout << "FCFS is a non preemptive method." << endl;
+    }
+    else if(Method == "SJF" && Mode == "Off")
+    {
+        SJF_NP(p,output);
+    }
+    else if(Method == "SJF" && Mode == "On")
+    {
+        cout << "This function is not implemented yet." << endl;
+    }
+    else if(Method == "Priority" && Mode == "Off")
+	{
+        priority_NP(p,output);
+    }
+    else if(Method == "Priority" && Mode == "On")
+    {
+        cout << "This function is not implemented yet." << endl;
+    }
+    else if(Method == "Round Robin" && Mode == "On")
+    {
+        cout << "This function is not implemented yet." << endl;
+    }
+    else if(Method == "Round Robin" && Mode == "Off")
+    {
+        cout << "FCFS is a non preemptive method."<< endl;
+    }
+    else if(Method == " " || Mode == " ")
+    {
+        cout << "Please choose scheduling method and preemptive mode." << endl;
+    }
+}
+
+void readOutputFile(const char* filename)
+{
+    FILE* OFile = fopen(filename,"r");
+    if (OFile != NULL)
+    {
+        char line [LINE_MAX];
+        while (fgets(line, LINE_MAX, OFile) != NULL)
+        {
+            cout << line << "\n";
+        }
+    }
+    else
+    {
+        cout << "Cannot open output.txt" << endl;
+    }
 } 
